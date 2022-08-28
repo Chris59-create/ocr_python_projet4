281fc5c7faad59367711def15528baa3bba08426
Chris Create <chris59create@gmail.com>
2022-08-28 22:30:39 +0200

    Complete and end the serialization and deserialization of the app datas through a controller db_manager and two classes, TableTournaments and Table_players

--- tournoisechecs/controllers/db_manager.py
+++ tournoisechecs/controllers/db_manager.py
@@ -3,6 +3,9 @@ from datetime import datetime
 from controllers.tournament_manager import TournamentManager
 from controllers.player_manager import PlayerManager
 from models.player import Player
+from models.match import Match
+from models.round import Round
+from models.tournament import Tournament
 
 db = TinyDB('db.json')
 tournaments_table = db.table("tournaments")
@@ -99,7 +102,96 @@ class TableTournament:
         tournaments_table.insert_multiple(serialized_tournaments)
 
     def install_tournament_data(self):
-        pass
+
+        serialized_tournaments = tournaments_table.all()
+
+        # Deserialization of tournaments
+        deserialized_tournaments = []
+        for serialized_tournament in serialized_tournaments:
+
+            # Deserialization of tournament rounds
+            serialized_tournament_rounds = serialized_tournament['tournament rounds']
+
+            deserialized_rounds = []
+            for serialized_round in serialized_tournament_rounds:
+
+                # Deserialization of matches
+                deserialized_matches = []
+
+                for serialized_match in serialized_round['matches']:
+
+                    player_score_1 = serialized_match[0]
+                    player_score_2 = serialized_match[1]
+
+                    serialized_player_1 = player_score_1['serialized_player']
+                    deserialized_player_1 = self.table_player.deserialize_player(serialized_player_1)
+                    score_player_1 = player_score_1['score_player']
+                    serialized_player_2 = player_score_2['serialized_player']
+                    deserialized_player_2 = self.table_player.deserialize_player(serialized_player_2)
+                    score_player_2 = player_score_2['score_player']
+
+                    deserialized_match = Match(deserialized_player_1,
+                                               score_player_1,
+                                               deserialized_player_2,
+                                               score_player_2
+                                               )
+
+                    deserialized_matches.append(deserialized_match)
+
+                # Deserialization of pair players
+                deserialized_pairs_players = []
+                for serialized_pair_players in serialized_round['pairs_players']:
+
+                    deserialized_player_0 = self.table_player.deserialize_player(
+                        serialized_pair_players['serialized_player_0']
+                    )
+                    deserialized_player_1 = self.table_player.deserialize_player(
+                        serialized_pair_players['serialized_player_1']
+                    )
+                    deserialized_pair_players = [deserialized_player_0, deserialized_player_1]
+
+                    deserialized_pairs_players.append(deserialized_pair_players)
+
+                deserialized_round = Round(serialized_round['round_name'],
+                                           deserialized_pairs_players,
+                                           serialized_round['start_date_time'],
+                                           serialized_round['end_date_time'],
+                                           deserialized_matches
+                                           )
+
+                deserialized_rounds.append(deserialized_round)
+
+            # Deserialization of tournament players
+            deserialized_tournament_players = []
+            for serialized_player in serialized_tournament['tournament players']:
+                deserialized_player = self.table_player.deserialize_player(serialized_player)
+
+                deserialized_tournament_players.append(deserialized_player)
+
+            # Deserialization of tournament final scores
+            deserialized_tournament_final_scores = []
+            for serialized_tournament_final_score in serialized_tournament['tournament final scores']:
+                serialized_player = serialized_tournament_final_score['player']
+                deserialized_player = self.table_player.deserialize_player(serialized_player)
+                deserialized_final_score = serialized_tournament_final_score['final_score']
+                deserialized_tournament_final_score = [deserialized_player, deserialized_final_score]
+
+                deserialized_tournament_final_scores.append(deserialized_tournament_final_score)
+
+
+            # Deserialization of all tournament
+            deserialized_tournament = Tournament(
+                serialized_tournament['tournament name'],
+                serialized_tournament['place'],
+                serialized_tournament['dates tournament'],
+                serialized_tournament['time control'],
+                serialized_tournament['tournament description'],
+                deserialized_rounds,
+                deserialized_tournament_players,
+                deserialized_tournament_final_scores
+            )
+
+            self.tournament_manager.tournaments_instances.append(deserialized_tournament)
 
 
 class TablePlayers:
--- tournoisechecs/models/round.py
+++ tournoisechecs/models/round.py
@@ -3,11 +3,11 @@ from datetime import datetime
 
 class Round:
 
-    def __init__(self, round_name, pairs_players):
+    def __init__(self, round_name, pairs_players, start_date_time=datetime.now(), end_date_time=None, matches=[]):
         self.round_name = round_name
-        self.start_date_time = datetime.now()
-        self.end_date_time = None
-        self.matches = []
+        self.start_date_time = start_date_time
+        self.end_date_time = end_date_time
+        self.matches = matches
         self.pairs_players = pairs_players
 
     def display_pairs_players(self):
